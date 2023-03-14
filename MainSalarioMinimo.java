7623a9fc3dbb264d5ac2ccb109f5bb2855433e61
Jesus Castañeda Espinosa <99563848+Akir91@users.noreply.github.com>
2023-03-14 09:48:41 -0600

    Update MainSalarioMinimo.java

--- MainSalarioMinimo.java
+++ MainSalarioMinimo.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class MainSalarioMinimo
 {
-   public static void main(String[] args)
+   public static void main(String [] args)
    {
-        //Introducir el codigo necesario para llamar la clase SalarioMinimo y resolver ejercicio 12
+       Scanner scanner = new Scanner(System.in);
+       SalarioMinimo salario1 = new SalarioMinimo();
+       
+       System.out.println("Programa que permite calcular el salario de un trabajador en la CDMX dadas las horas totales de trabajo");
+       
+       System.out.print("\nIntroduce la cantidad de horas totales: ");
+       int h = scanner.nextInt();
+       salario1.setHorasTrabajoMes(h);
+       
+       System.out.println("\nEl salario a pagar por " + h + " horas de trabajo es $" + salario1.getSalario() +".");
+       System.out.println("NOTA: Considerando el salario minimo por 8 horas de trabajo en CDMX = " + salario1.getSalarioMinimoCDMX());       
    }
 }
